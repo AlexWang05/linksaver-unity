bfad2ff8ff15f40b307b83abe3a5ae6b9c75163f
alexwang05 <hpwzz20@gmail.com>
2021-07-28 20:21:59 -0700

    edit buttons working with launch

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AppManager : MonoBehaviour
 {
+    public GameObject AddCoursePanel;
+    private int buttonNumber;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (AddCoursePanel != null)
+        {
+            AddCoursePanel.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +23,27 @@ public class AppManager : MonoBehaviour
     {
         
     }
+
+    // gets the name of the button that was clicked
+    int GetCurrentObjectName()
+    {
+        // gets name of button
+        string objectName = EventSystem.current.currentSelectedGameObject.name;
+
+        // gets int value of last character in name
+        return (int)Char.GetNumericValue(objectName[objectName.Length - 1]);
+    }
+
+    // onClick function for edit buttons
+    public void LaunchAddCourseWindow()
+    {
+        buttonNumber = GetCurrentObjectName();
+
+        if (AddCoursePanel != null)
+        {
+            AddCoursePanel.SetActive(true);
+        }
+    }
+
+
 }
