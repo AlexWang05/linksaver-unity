fa5ae789a3a93b9a136e3123d6f59ca030a23709
Alex Wang <73456512+AlexWang05@users.noreply.github.com>
2022-12-30 15:44:39 -0800

    add login interface (no web yet)

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    
+    // assigned via editor
+    public TMP_InputField emailField, passwordField;
+    public TMP_Text emailText;
+    public TMP_Text passwordText;
+
+    private int inputSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tab and shift-tab switching
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inputSelected == 0)
+            {
+                inputSelected++;
+            }
+            else if (inputSelected == 1)
+            {
+                inputSelected--;
+            }
+            else
+            {
+                Debug.Log("Input Selected: " + inputSelected);
+            }
+            // inputSelected++;
+            // if (inputSelected > 1) inputSelected = 0;
+            selectInputField();
+        }
+
+        void selectInputField()
+        {
+            switch (inputSelected)
+            {
+                case 0:
+                    emailField.Select();
+                    break;
+                case 1:
+                    passwordField.Select();
+                    break;
+            }
+        }
+    }
+
+    public void emailInputSelected() => inputSelected = 0;
+    public void passwordInputSelected() => inputSelected = 1;
+
+    // OnClick() Function for Log In Button
+    public void LaunchApp()
+    {
+        // save player data
+        string email = emailField.text;
+        string password = passwordField.text;
+
+        
+
+        // launch main screen
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
