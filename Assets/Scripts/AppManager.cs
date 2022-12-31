f0fe9e0b22932b46f9c30ed14393406c2596b578
Alex Wang <73456512+AlexWang05@users.noreply.github.com>
2022-12-30 17:28:06 -0800

    full online storage

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -11,15 +11,20 @@ public class AppManager : MonoBehaviour
     public GameObject AddCoursePanel;
     private int buttonNumber;
     private int inputSelected;
+    private string email, password;
     
     // assigned via editor
     public TMP_InputField nameInputField, courseLinkInputField, meetingLinkInputField;
     public TMP_Text[] courseName = new TMP_Text[8];
     public TMP_Text[] courseInputText = new TMP_Text[3];
 
+    [SerializeField] string formURL;
+
     // Start is called before the first frame update
     void Start()
     {
+        formURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSciADlpfu34jO7ZlhNMWkYGDJoaUtWfUb7529GP3XQ6W8pLZQ/formResponse";
+
         // starts by hiding add course panel to show main panel
         if (AddCoursePanel)
         {
@@ -149,6 +154,43 @@ public class AppManager : MonoBehaviour
 
         Application.OpenURL(courseLink);
         Application.OpenURL(meetingLink);
+    }
+
+    IEnumerator Post(string email, string password, string one, string two, string three, string four, string five, string six, string seven, string eight)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1379215087", email);
+        form.AddField("entry.1252330804", password);
+        form.AddField("entry.947268921", one);
+        form.AddField("entry.1736639115", two);
+        form.AddField("entry.331514112", three);
+        form.AddField("entry.1389814241", four);
+        form.AddField("entry.394189714", five);
+        form.AddField("entry.639743565", six);
+        form.AddField("entry.577703527", seven);
+        form.AddField("entry.1076159465", eight);
+
+        UnityWebRequest www = UnityWebRequest.Post(formURL, form);
+        yield return www.SendWebRequest();
+    }
+
+    void OnApplicationQuit()
+    {
+        // get all course names
+        string name_1 = courseName[0].text;
+        string name_2 = courseName[1].text;
+        string name_3 = courseName[2].text;
+        string name_4 = courseName[3].text;
+        string name_5 = courseName[4].text;
+        string name_6 = courseName[5].text;
+        string name_7 = courseName[6].text;
+        string name_8 = courseName[7].text;
+
+        email = PlayerPrefs.GetString("email");
+        password = PlayerPrefs.GetString("password");
+
+        // post to database
+        StartCoroutine(Post(email, password, name_1, name_2, name_3, name_4, name_5, name_6, name_7, name_8));
     }
 
 }
